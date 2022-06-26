af84108b500d9a44fba81cbdac57db47af537939
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-06-26 11:04:43 +0530

    Setting up Retrofit.

---
+++ app/src/main/java/tech/developingdeveloper/printme/core/data/RetrofitClient.kt
@@ -0,0 +1,35 @@
+package tech.developingdeveloper.printme.core.data
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class RetrofitClient {
+
+    private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    fun getClient(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
+            .build()
+    }
+
+    companion object {
+        private const val BASE_URL = "http://192.168.1.100:8081/"
+    }
+}
